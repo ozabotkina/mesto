a6940fa7e6054fe615369c84ee9579874bb61adb
Olga Bavykina <ozabotkina2@yandex.ru>
2022-05-15 10:49:25 +0300

    for check

---
+++ index.js
@@ -0,0 +1,39 @@
+let popup = document.querySelector ('.popup');
+let editButton = document.querySelector ('.edit-button');
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+}
+editButton.addEventListener ('click', openPopup);
+
+
+
+let closeButton = document.querySelector ('.popup__close-icon');
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+closeButton.addEventListener ('click', closePopup);
+
+
+let authorName = document.querySelector('.profile__author_name');
+let nameInput = document.querySelector('.popup__author_name');
+nameInput.value = authorName.textContent;
+
+let authorAbout = document.querySelector('.profile__author_about');
+let jobInput = document.querySelector('.popup__author_about');
+jobInput.value = authorAbout.textContent;
+
+
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__container');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+   authorName.textContent = nameInput.value; 
+   authorAbout.textContent = jobInput.value;
+   closePopup();
+   }
+
+   formElement.addEventListener('submit', formSubmitHandler);

0b50b9acc8a5bd934216b71564af237ae47b6cdb
Olga Bavykina <ozabotkina2@yandex.ru>
2022-05-24 16:21:41 +0300

    first cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 const popup = document.querySelector ('.popup');
 const editButton = document.querySelector ('.edit-button');
 const closeButton = document.querySelector ('.popup__close-icon');
@@ -6,6 +7,7 @@ const nameInput = document.querySelector('.popup__name');
 const authorAbout = document.querySelector('.profile__about');
 const jobInput = document.querySelector('.popup__about');
 const formElement = document.querySelector('.popup__container');
+
 
 
 function openPopup() {
@@ -32,7 +34,54 @@ closeButton.addEventListener ('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
 
+// НОВОЕ
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+
+function addCard(link, name) {
+const cardTemplate = document.querySelector('#card-element').content;
+const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+const containerElements = document.querySelector('.elements');
+const cardImage = cardElement.querySelector('.element__image');
+cardImage.src = link;
+const cardTitle = cardElement.querySelector('.element__title')
+cardTitle.textContent = name;
+containerElements.append(cardElement);
+};
+
+function addInitialCards() {
+for (let i = 0; i < 6; i++) {
+addCard(initialCards[i].link, initialCards[i].name);
+}
+};
+
+addInitialCards();
 
 
 

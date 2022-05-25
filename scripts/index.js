eb0a98c0953887bac431b5d4bbfdecf41798eb0b
Olga Bavykina <ozabotkina2@yandex.ru>
2022-05-25 07:01:21 +0300

    added new cards form

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,48 @@
 
 const popup = document.querySelector ('.popup');
 const editButton = document.querySelector ('.edit-button');
-const closeButton = document.querySelector ('.popup__close-icon');
+const closeButtonAuthor = document.querySelector ('.popup__close-icon_author');
 const authorName = document.querySelector('.profile__name');
 const nameInput = document.querySelector('.popup__name');
 const authorAbout = document.querySelector('.profile__about');
 const jobInput = document.querySelector('.popup__about');
 const formElement = document.querySelector('.popup__container');
+const popupAuthor = document.querySelector('.popup_author');
+const popupNewCard = document.querySelector('.popup_new-card');
+const addButton = document.querySelector('.add-button');
+const closeButtonNewCard = document.querySelector ('.popup__close-icon_new-card');
 
-
-
-function openPopup() {
-  popup.classList.add('popup_opened');
-  nameInput.value = authorName.textContent;
-  jobInput.value = authorAbout.textContent;
-
+function openPopup(name) {
+  name.classList.add('popup_opened');
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function closePopup(name) {
+  name.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
   authorName.textContent = nameInput.value; 
   authorAbout.textContent = jobInput.value;
-  closePopup();
+  closePopup(popupAuthor);
 }
 
+addButton.addEventListener ('click', function() {
+  openPopup(popupNewCard);
+});
 
-editButton.addEventListener ('click', openPopup);
-closeButton.addEventListener ('click', closePopup);
+
+editButton.addEventListener ('click', function() {
+  openPopup(popupAuthor)
+  nameInput.value = authorName.textContent;
+  jobInput.value = authorAbout.textContent;
+});
+
+closeButtonAuthor.addEventListener ('click', function() {closePopup(popupAuthor)});
+closeButtonNewCard.addEventListener ('click', function() {closePopup(popupNewCard)});
+
 formElement.addEventListener('submit', formSubmitHandler);
 
-
-// НОВОЕ
 
 const initialCards = [
   {

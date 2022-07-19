951719cecaaf2de996a4415a482f9307ef0ed2e8
Olga Bavykina <ozabotkina2@yandex.ru>
2022-07-19 16:37:20 +0300

    sprint eight

--- src/components/Сard.js
+++ src/components/Сard.js
@@ -1,6 +1,4 @@
 
-import { PopupWithImage } from "./PopupWithImage.js";
-import { createImagePopup } from "./shared.js";
 
 export class Card {
     constructor(link, name, cardSelector, {handleCardClick}){
@@ -54,77 +52,3 @@ export class Card {
 
 
 
-// import { popupImage, imageInPopup, commentInPopup, closeInPopup, closePopup, openPopup } from "./shared.js"; 
-
-// export class Card { 
-//     constructor(link, name, cardSelector){ 
-//       this._image = link; 
-//       this._title = name; 
-//       this._cardSelector = cardSelector; 
-//     }; 
-
-   
-
-//     _getTemplate() { 
-//       const cardElement = document 
-//       .querySelector(this._cardSelector) 
-//       .content 
-//       .querySelector('.element') 
-//       .cloneNode(true); 
-
-//       return cardElement; 
-//     } 
-
-   
-
-//     generateCard() { 
-
-//       this._element = this._getTemplate(); 
-//       this._setEventListeners(); 
-//       this._imageSelector.src = this._image; 
-//       this._imageSelector.alt = this._title; 
-//       this._element.querySelector('.element__title').textContent = this._title;    
-//       return this._element; 
-//     } 
-
-     
-
-//     _setEventListeners() { 
-
-//       this._imageSelector = this._element.querySelector('.element__image'); 
-//       this._element.querySelector('.element__like').addEventListener('click', () => {this._like()}); 
-//       this._element.querySelector('.element__trash').addEventListener('click', () => {this._sendToTrash()}); 
-//       this._imageSelector.addEventListener('click', () => {this._createImagePopup()}); 
-
-//     } 
-
-   
-
-//     _like() { 
-//       this._element.querySelector('.element__like').classList.toggle('element__like_active'); 
-//     }; 
-
-   
-
-//     _sendToTrash() { 
-
-//       this._element.remove(); 
-//       this._element = null; 
-
-//     }; 
-
-   
-
-//     _createImagePopup() { 
-
-//       imageInPopup.src = this._image; 
-//       imageInPopup.alt = this._title; 
-//       commentInPopup.textContent = this._title; 
-//       openPopup(popupImage); 
-
-//     }; 
-//   } 
-
-   
-
- 
